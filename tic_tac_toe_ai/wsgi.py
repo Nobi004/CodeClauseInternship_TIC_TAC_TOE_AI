9bf6bd2858fd3d83124020fe7522e8b7b0bc6d1f
Nobi004 <148976917+Nobi004@users.noreply.github.com>
2024-11-13 18:47:15 +0600

    deployment stage

--- tic_tac_toe_ai/wsgi.py
+++ tic_tac_toe_ai/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tic_tac_toe_ai.settings")
 
 application = get_wsgi_application()
+
+app = application
